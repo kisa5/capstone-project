9f1c17d7b6905c59418250ad3ea830c1ed9e8252
Anna Hoehl <anna.hoehl@curanta.de>
2023-01-20 18:20:47 +0100

    install @svgr/webpack and add logo to the header

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ProjectItem from "components/ProjectItem";
+import Logo from "components/svg/logo.svg";
 
 export default function HomePage() {
   return (
     <>
       <header>
+        <Logo alt="logo" height={50} width={50} />
         <h1>my projects</h1>
       </header>
       <main>
