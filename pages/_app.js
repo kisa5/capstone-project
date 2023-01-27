c4e6d5746c3d223e74dd9461e54ab852991ccbbb
Anna Hoehl <anna.hoehl@curanta.de>
2023-01-27 10:03:16 +0100

    update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,23 @@
 import GlobalStyle from "@/styles";
 import Head from "next/head";
+import useLocalStorageState from "use-local-storage-state";
 
 export default function App({ Component, pageProps }) {
+  const [project, setProject] = useLocalStorageState("project", {
+    defaultValue: [],
+  });
+
+  function addProject(projectname) {
+    setProject([projectname, ...project]);
+  }
+
   return (
     <>
       <GlobalStyle />
       <Head>
         <title>Capstone Project</title>
       </Head>
-      <Component {...pageProps} />
+      <Component {...pageProps} addProject={addProject} project={project} />
     </>
   );
 }
