757eba9e6c5b7e770bc146257e964eba3c221e75
Anna Hoehl <anna.hoehl@curanta.de>
2023-02-02 16:28:47 +0100

    add styling to the DeleteModal component

--- components/ModalDelete.js
+++ components/ModalDelete.js
@@ -12,13 +12,52 @@ export default function ModalDelete({
   }
   return (
     <Modal appear={appearModalDelete}>
-      <div>
-        <h1>Do you really want to delete your project?</h1>
-        <div>
-          <button onClick={handleClose}>No</button>
-          <button oClick={deleteAndClose}>Yes</button>
-        </div>
-      </div>
+      <StyledCard>
+        <Question>Do you really want to delete your project?</Question>
+        <Wrapper>
+          <StyledButton onClick={handleClose}>No</StyledButton>
+          <StyledButton oClick={deleteAndClose}>Yes</StyledButton>
+        </Wrapper>
+      </StyledCard>
     </Modal>
   );
 }
+
+const StyledCard = styled.div`
+  display: flex;
+  align-items: flex-start;
+  justify-items: center;
+  background-color: rgb(250, 250, 250);
+  width: 80%;
+  height: 50%;
+  border-radius: 5px;
+  padding: 10px;
+`;
+
+const Question = styled.h1`
+  text-align: center;
+  font-size: 18px;
+  color: #212121;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  gap: 20px;
+`;
+
+const StyledButton = styled.button`
+  display: inline-block;
+  outline: 0;
+  cursor: pointer;
+  border: none;
+  padding: 0 56px;
+  height: 45px;
+  line-height: 45px;
+  border-radius: 7px;
+  font-weight: 400;
+  font-size: 16px;
+  background: #fff;
+  color: #696969;
+  box-shadow: 0 4px 14px 0 rgb(0 0 0 / 10%);
+  transition: background 0.2s ease, color 0.2s ease, box-shadow 0.2s ease;
+`;
